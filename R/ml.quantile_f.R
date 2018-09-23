7d296fc7f9c054a265cba6800382b7612d5dbcfc
Alex Keil <alex.keil@gmail.com>
2018-09-23 00:18:22 -0400

    unique breaks

--- R/ml.quantile_f.R
+++ R/ml.quantile_f.R
@@ -3,7 +3,7 @@ function (data, v_n, breaks){
     for (i in 1:length(v_n)) {
         dat_num = as.numeric(unlist(data[, v_n[i]]))
         data[[paste(v_n[i], "q", sep = "_")]] = cut(dat_num, 
-            breaks = breaks[,i], labels = FALSE, 
+            breaks = unique(breaks[,i]), labels = FALSE, 
             include.lowest = TRUE) - 1
     }
     return(data)
